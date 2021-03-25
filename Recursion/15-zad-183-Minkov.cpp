60b9a1bbdcc9891bad1d5c71339b853d14e921df
Dimitar I. Damyanov <dim.damianov@gmail.com>
2021-03-25 11:36:34 +0200

    Added solution

---
+++ Recursion/15-zad-183-Minkov.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void inputArray(int *a, int n) {
+    if (n == 0) {
+        return;
+    }
+    else {
+        std::cout << "a[" << n - 1 << "]= ";
+        std::cin >> *(a + n -1);
+        inputArray(a, n-1);
+    }
+}
+
+void evenElementsAverage(int *a, int n, int *sum, int *count) {
+    if (n == 0)
+        return;
+    if (*(a + n - 1) % 2 == 0) {
+        *sum += *(a + n - 1);
+        *count++;
+    }
+    evenElementsAverage(a, n - 1, sum, count);
+}
+
+int main() {
+    int n, sum=0, count=0;
+    std::cout << "n= ";
+    std::cin >> n;
+    int a[n];
+    inputArray(a, n);
+    evenElementsAverage(a, n, &sum, &count);
+    std::cout << "Even elements average= " << (float) sum / count << std::endl;
+    return 0;
+}
