9d5e6ecd185c3f4459bafdd6fda85f4ae3196a7f
Dimitar I. Damyanov <dim.damianov@gmail.com>
2021-03-25 10:34:49 +0200

    Change style to Minkov C++

--- Recursion/12-zad-Minkov.cpp
+++ Recursion/12-zad-Minkov.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-void sumToNegative(int *a, int n, int sum=0) {
+void sumToNegative(int *a, int n, int *sum) {
     if (*(a + n - 1) < 0) {
-        std::cout << sum << std::endl;
+        std::cout << *sum << std::endl;
         return;
     }
     else if (n == 0) {
         std::cout << "No negatives in array" << std::endl;
     }
     else {
-        sum += *(a + n - 1);
+        *sum += *(a + n - 1);
         sumToNegative(a, n-1, sum);
     }
 }
@@ -29,6 +29,7 @@ void inputArray(int *a, int n ) {
 int main() {
     int a[5];
     inputArray(a, 5);
-    sumToNegative(a, 5);
+    int sum = 0;
+    sumToNegative(a, 5, &sum);
     return 0;
 }
